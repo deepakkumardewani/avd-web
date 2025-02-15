0ad52eaa58167fd86418cdbc6ea2f19811490004
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2025-02-15 10:33:24 +0530

    ADD: types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Audio {
+  _id: string
+  title: string
+  subTitle: string
+  url: string
+}
+
+export interface Video {
+  id: string
+  title: string
+  thumbnail: string
+  viewCount: number
+  publishedAt: string
+}
